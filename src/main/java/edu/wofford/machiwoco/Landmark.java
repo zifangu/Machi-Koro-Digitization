ac629472b2074ac71291d053f5caf4001c86eea0
Ivan Gu <guz@email.wofford.edu>
2020-10-09 14:05:58 -0400

    working progress

--- src/main/java/edu/wofford/machiwoco/Landmark.java
+++ src/main/java/edu/wofford/machiwoco/Landmark.java
@@ -41,30 +41,56 @@ public class Landmark extends Card{
         this.icon_ab = icon_ab;
     }
 
+    //  return strings to be used in generateLine()
+    public String generate_single_line(String s) {
+        String str = StringUtils.center(s, 21, " ");
+        return "| " + str + " |\n";
+    }
+
+    public String[] generate_blocks(String s) {
+        int len = s.length();
+        System.out.println("slen: " + len);
+        // determines how many lines to print out
+        // if s is longer than 23 char
+        int new_lines = len / 23 + 1;
+        String[] stringsToProcess = new String[new_lines];
+        int start_index = 0;
+        int end_index;
+
+        for (int i = 0; i < new_lines; i++) {
+//            this will be a multiline string
+            for (int j = 20; j > 0; j--) {
+                if (i == new_lines - 1) {j = len;}
+                if (s.charAt(j) == ' ') {
+                    end_index = j;
+                    stringsToProcess[i] = s.substring(start_index, end_index);
+                    s = s.substring(end_index + 1, len);
+                }
+            }
+        }
+        return stringsToProcess;
+    }
+
     //  return strings to be used in toString()
     public String generateLine(String s) {
-        String str = StringUtils.center(s, 21, " ");
-
-        // description construction
-        String des_cons;
         int len = s.length();
         System.out.println("slen: " + len);
 
-
-        // determines how many new lines to print out the description
+        // determines how many lines to print out
+        // if s is longer than 23 char
         int new_lines = len / 23 + 1;
         for (int i = 0; i < new_lines; i++) {
-            System.out.println("len:" + new_lines);
-
 //            this will be a multiline string
             if (new_lines > 1) {
                 for (int j = 20; j > 0; j--) {
 // TODO
+                        if (s.charAt(j) == ' ') {
+
+                        }
                     int k = 0;
                 }
             } else {
                 System.out.println("ppp");
-                return "| " + str + " |\n";
             }
         }
         return "";
