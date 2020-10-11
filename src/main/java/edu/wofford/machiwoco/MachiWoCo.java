c1c133b496ea194be78471db6201297378602b67
Ivan Gu <guz@email.wofford.edu>
2020-10-11 15:31:13 -0400

    working progress

--- src/main/java/edu/wofford/machiwoco/MachiWoCo.java
+++ src/main/java/edu/wofford/machiwoco/MachiWoCo.java
@@ -8,8 +8,11 @@ public class MachiWoCo {
     public static void main(String[] args) {
 
         System.out.println("Let's play Machi WoCo!");
-//        Landmark.main(args);
+        if (args[0].equals("landmark")) {
+        Landmark.main(args);
+        } else if (args[0].equals("establishment")) {
         Establishment.main((args));
+        }
     }
 
 }
