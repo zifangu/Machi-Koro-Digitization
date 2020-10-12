eafc4f497c8d1deabe08bec7eaf92d64f3810118
ecraft <ecraft@visiostack.com>
2020-10-12 13:42:48 -0400

    API Card

--- src/main/java/edu/wofford/machiwoco/Card.java
+++ src/main/java/edu/wofford/machiwoco/Card.java
@@ -6,6 +6,15 @@ import org.apache.commons.lang3.StringUtils;
 ////            "cost": 1,
 ////            "color": "blue",
 ////            "icon": "wheat",
+
+/**
+ * This is a class that represents the basics of a Machi Koro card - both for Establishment and Landmark.
+ * 
+ * @author Eric Craft
+ * @author Ivan Gu
+ * @author Bennett Joyce
+ */
+
 public class Card {
      //Initial Enums For Color and Icons
      protected enum Color {BLUE, GREEN, PURPLE, RED, NONE};
@@ -22,6 +31,15 @@ public class Card {
      protected Color_ab color_ab;
      protected  Icon_ab icon_ab;
 
+     /**
+      * Card constructor
+      * @param name is the name of the card itself
+      * @param cost is a 1-2 digit number representing the cost of the card
+      * @param color is the color of the card in enumeration form
+      * @param icon is the icon on the card in enumeration form
+      * @param description is the action description of the card
+      */
+
      public Card(String name, int cost, Color color, Icon icon, String description) {
           this.name = name;
           this.color = color;
@@ -30,37 +48,83 @@ public class Card {
           this.description = description;
      }
 
+     /**
+      * Gets the card's name
+      * @return a <code> string </code> specifying the card's name
+      */
+
      public String getName() {
           return name;
      }
+
+     /**
+      * Sets the card's name
+      * @param name the card's name
+      */
 
      public void setName(String name) {
           this.name = name;
      }
 
+     /**
+      * Gets the card's color
+      * @return an <code> enumeration value </code> specifying the card's color
+      */
+
      public Color getColor() {
           return color;
      }
+
+     /**
+      * Gets the card's cost
+      * @return an <code> integer </code> specifying the card's cost
+      */
 
      public int getCost() {
           return cost;
      }
 
+     /**
+      * Sets the card's cost
+      * @param cost the card's cost
+      */
+
      public void setCost(int cost) {
           this.cost = cost;
      }
+
+     /**
+      * Gets the card's icon
+      * @return an <code> enumeration value </code> specifying the card's icon
+      */
 
      public Icon getIcon() {
           return icon;
      }
 
+     /**
+      * Gets the card's description
+      * @return a <code> string </code> specifying the card's action description
+      */
+
      public String getDescription() {
           return description;
      }
 
+     /**
+      * Sets the card's description
+      * @param description the card's action description
+      */
+
      public void setDescription(String description) {
           this.description = description;
      }
+
+     /**
+      * Returns a string line to be used in the card's toString() method
+      * @param s the string to be centered in console output
+      * @return a <code> string </code> representing a line in the card's output to the console
+      */
 
      //  return strings to be used in toString()
      public String generate_single_line(String s) {
