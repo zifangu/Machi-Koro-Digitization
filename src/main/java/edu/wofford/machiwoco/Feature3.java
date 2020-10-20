e866bf3ec3ab0f06e856d5cc30f34463a2b7edef
Ivan Gu <guz@email.wofford.edu>
2020-10-20 00:04:58 -0400

    Feature 3 update

---
+++ src/main/java/edu/wofford/machiwoco/Feature3.java
@@ -0,0 +1,36 @@
+package edu.wofford.machiwoco;
+
+import org.apache.commons.lang3.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Stream;
+
+/**
+ * phase 1 of the game with 1(one) random AI
+ *
+ * @author Eric Craft
+ * @author Ivan Gu
+ * @author Bennett Joyce
+ */
+
+
+public class Feature3 extends Feature2 {
+//    Constructor
+    public Feature3() {
+        super();
+    }
+
+
+    @Override
+    public void playGame() {
+        super.playGame();
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello AI");
+    }
+}
---
+++ src/test/java/edu/wofford/machiwoco/Feature3Test.java
@@ -0,0 +1,34 @@
+package edu.wofford.machiwoco;
+
+import io.cucumber.java.jv.Lan;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class Feature3Test  {
+    Feature3 feature3;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void before() {
+        feature3 = new Feature3();
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    public void testPlayerNum() {
+        assertThat(feature3.getPlayers().length, is(2));
+    }
+
+}
