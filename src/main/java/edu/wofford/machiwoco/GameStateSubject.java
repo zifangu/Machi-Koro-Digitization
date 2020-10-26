3b1bd41d3117cadfe7ecf48248e532a30718d69a
cameronjoyce <joycecb@email.wofford.edu>
2020-10-25 22:47:26 -0400

    More Comments

--- src/main/java/edu/wofford/machiwoco/GameStateSubject.java
+++ src/main/java/edu/wofford/machiwoco/GameStateSubject.java
@@ -18,35 +18,65 @@ public class GameStateSubject {
         this.players = players;
         this.market = market;
     }
-
+    /**
+     * Gets Establishment Order
+     * @return Array of correctly-ordered eEstablishments
+     */
     public Establishment[] getEstOrder() {
         return EST_ORDER;
     }
 
+    /**
+     * Sets correctly-ordered array of Establishment Order
+     * @param EST_ORDER Array of ordered Establishments
+     */
     public void setEstOrder(Establishment[] EST_ORDER) {
         this.EST_ORDER = EST_ORDER;
     }
 
+    /**
+     * Gets array of all players
+     * @return Array of players
+     */
     public Player[] getPlayers() {
         return players;
     }
 
+    /**
+     * Sets correctly-ordered array of Players
+     * @param players array of players in current game
+     */
     public void setPlayers(Player[] players) {
         this.players = players;
     }
 
+    /**
+     * Gets Hashmap of Market
+     * @return Hashmap of Market
+     */
     public Map<Establishment,Integer> getMarket() {
         return market;
     }
 
+    /**
+     * Sets Hashmap of Market
+     * @param  market Hashmap of market
+     */
     public void setMarket(Map<Establishment,Integer> market) {
         this.market = market;
     }
 
+    /**
+     * Attaches observer to subject
+     * @param observer observer to attach to subject
+     */
     public void attach(edu.wofford.machiwoco.Observer observer){
         observers.add(observer);
     }
 
+    /**
+     * Notifies observers when action occurs
+     */
     public void notifyObservers(){
         for (Observer observer : observers) {
             observer.update();
