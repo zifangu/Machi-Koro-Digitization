c1ccc0033cc3ac61931a8fc4cb6a3e590f46c6ce
Ivan Gu <guz@email.wofford.edu>
2020-11-11 20:58:59 -0500

    feature6 test init

---
+++ src/test/java/edu/wofford/machiwoco/Feature6Test.java
@@ -0,0 +1,34 @@
+package edu.wofford.machiwoco;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+import org.junit.*;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+
+public class Feature6Test {
+    Feature6 feature6;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void before() {
+        feature6 = new Feature6(3);
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+
+}
